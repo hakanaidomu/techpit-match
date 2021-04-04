f2232f4651312b7770f8720988468602a24f96c2
sasaki <hakanaidomu@gmail.com>
2021-04-04 09:11:22 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
          has_many :reactions
          has_many :chat_room_users
          has_many :chat_rooms, through: :chat_room_users
+         has_many :chat_messages
          
          def update_without_current_password(params, *options)
 
